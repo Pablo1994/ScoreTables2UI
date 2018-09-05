c2324a68e83913ead4637a0bb81e4d5f741e3b42
suzethi <s.jackie.b@gmail.com>
2018-09-05 17:57:15 -0600

    Avances día 1 tabla

---
+++ ScoreTablesUI/src/components/Table.vue
@@ -0,0 +1,38 @@
+<template>
+    <vue-table :columns="columns" :rows="rows"></vue-table>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        // columns definition only requires a name to be displayed
+        columns: [
+          {
+            name: "name"
+          },{
+            name: "nickname"
+          },{
+            name: "company"
+          }
+        ],
+        // result set
+        rows: [
+          {
+            name: "Robert Duvak",
+            nickname: "jose26",
+            company: "Apple Inc."
+          },{
+            name: "Isabelle Champion",
+            nickname: "isa",
+            company: "EDF"
+          },{
+            name: "Alberto Tulador",
+            nickname: "beber",
+            company: "Matla"
+          }
+        ]
+      }
+    }
+  }
+</script>
--- ScoreTablesUI/src/main.js
+++ ScoreTablesUI/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueTable from '@lossendae/vue-table'
 
+// install globally...
+Vue.use('vue-table', VueTable)
 Vue.config.productionTip = false
 
 new Vue({
